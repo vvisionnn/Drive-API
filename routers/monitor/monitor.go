8b559033af4773d550c19ff5b8349252eb5cc345
vvisionnn <wz546500392@gmail.com>
2021-02-10 20:40:22 +0800

    add cache builder beta

--- routers/monitor/monitor.go
+++ routers/monitor/monitor.go
@@ -3,7 +3,9 @@ package monitor
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/vvisionnn/Drive-API/pkgs/onedrive"
 	"github.com/vvisionnn/Drive-API/pkgs/response"
+	"github.com/vvisionnn/Drive-API/routers/drive"
 	"time"
 )
 
@@ -18,5 +20,47 @@ func CachePing(ctx *gin.Context) {
 // api for test
 func CacheHandler(ctx *gin.Context) {
 	// recursive get folder
+	// list root, get all folder's id
+	// BFS get children
+	items, err := drive.Drive.ListRootChildren()
+	if err != nil {
+		response.InternalServerError(ctx, err.Error())
+		return
+	}
+	var result []*Node
+	for _, item := range items.Value {
+		node := &Node{
+			item,
+			[]*Node{},
+		}
+		node.BFSGetChildren()
+		result = append(result, node)
+	}
+	fmt.Println(result)
 	response.Success(ctx)
+}
+
+type Node struct {
+	onedrive.ItemInfo
+	children []*Node
+}
+
+func (node *Node) BFSGetChildren() {
+	fmt.Println("id: ", node.ID, "name: ", node.Name)
+	items, err := drive.Drive.ListItemChildren(node.ID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, item := range items.Value {
+		node.children = append(node.children, &Node{
+			item,
+			[]*Node{},
+		})
+	}
+
+	for _, child := range node.children {
+		child.BFSGetChildren()
+	}
 }
